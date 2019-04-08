2d970a6ce5210c65eb51033ed9df8f4fad66f7a0
christopherchoe <chchchoe@gmail.com>
2019-04-08 19:16:34 +0000

    added simple content to index.html file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,6 +5,7 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 sudo touch /data/web_static/releases/test/index.html
+echo "simple content" | sudo tee -a /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/relases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i 's|/usr/share/nginx/html|/data/web_static/current|' /etc/nginx/sites-enabled/default
