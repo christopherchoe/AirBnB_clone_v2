3f34dbd491b378d87ac417375ad5a578b18d5066
christopherchoe <chchchoe@gmail.com>
2019-04-08 18:16:49 +0000

    initial commit

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# sets up server nginx and directories
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/current
+sudo touch /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/relases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i 's|/usr/share/nginx/html|/data/web_static/current' /etc/nginx/sites-enabled/default
+sudo service nginx restart
