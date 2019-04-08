ff84ebd3adb7b2410b989250565b0e516ab779ab
christopherchoe <chchchoe@gmail.com>
2019-04-08 20:00:03 +0000

    fixed typo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,7 @@ sudo mkdir -p /data/web_static/shared
 sudo touch /data/web_static/releases/test/index.html
 echo "simple content" | sudo tee -a /data/web_static/releases/test/index.html
 sudo rm -rf /data/web_static/current
-sudo ln -sf /data/web_static/relases/test /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "/^}/i location /web_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
