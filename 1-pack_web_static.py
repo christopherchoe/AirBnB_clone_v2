cb26d1c03026d0686dd426d3189fe4c8bb9558b9
christopherchoe <chchchoe@gmail.com>
2019-04-08 22:00:40 +0000

    added file extension

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,7 +11,7 @@ def do_pack():
     packs content of web_static/ into a tgz archive
     """
     now = datetime.datetime.now()
-    tgz_path = 'versions/web_static_' + now.strftime('%Y%m%d%H%M%S')
+    tgz_path = 'versions/web_static_' + now.strftime('%Y%m%d%H%M%S') + '.tgz'
     tgz_cmd = 'tar -czpvf ' + tgz_path + ' web_static'
     local('sudo mkdir -p versions')
 
