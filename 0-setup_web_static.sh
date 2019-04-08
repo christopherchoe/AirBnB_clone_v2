45036bb422bea2ad1cecd27c40722620f5d14209
christopherchoe <chchchoe@gmail.com>
2019-04-08 20:09:13 +0000

    fixed web to hbnb

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,5 +9,5 @@ echo "simple content" | sudo tee -a /data/web_static/releases/test/index.html
 sudo rm -rf /data/web_static/current
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "/^}/i location /web_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
+sudo sed -i "/^}/i location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
