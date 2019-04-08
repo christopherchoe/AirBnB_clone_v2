7a30b6423fd3ab0ff2fdaa2d4eeccac45d8d1853
christopherchoe <chchchoe@gmail.com>
2019-04-08 21:44:24 +0000

    fixed pep8 and path issue

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-generates .tgz archive from /data/web_static/ using do_pack
+generates .tgz archive from web_static/ using do_pack
 """
 from fabric.api import *
 import datetime
@@ -8,13 +8,14 @@ import datetime
 
 env.hosts = ['34.73.105.249']
 
+
 def do_pack():
     """
-    packs content of /data/web_static/ into a tgz archive
+    packs content of web_static/ into a tgz archive
     """
     now = datetime.datetime.now()
     tgz_path = 'web_static_' + now.strftime('%y%m%d%H%M%S')
-    tgz_cmd = 'tar -czpvf versions/' + tgz_path + ' /data/web_static'
+    tgz_cmd = 'tar -czpvf versions/' + tgz_path + ' web_static'
     local('sudo mkdir -p versions')
 
     if local(tgz_cmd).succeeded:
