c98f43a32408d4510f5e52e73335b3591c4b4902
christopherchoe <chchchoe@gmail.com>
2019-04-24 07:11:56 +0000

    initial commit

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application, with two different slashes
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    displays Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    display HBNB
+    """
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run()
