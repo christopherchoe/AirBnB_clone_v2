baef231a27b89771b284ab60ca096cb29b747085
christopherchoe <chchchoe@gmail.com>
2019-04-24 17:05:48 +0000

    initial commit

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application, with two different slashes
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    displays Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    display HBNB
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    display C followed by text variable
+    """
+    return 'C %s' % str(text).replace('_', ' ')
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """
+    display Python followed by text variable
+    """
+    return 'Python %s' % str(text).replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """
+    display n if n is an integer
+    """
+    try:
+        return '%d is a number' % n
+    except TypeError:
+        pass
+
+if __name__ == '__main__':
+    app.run()
