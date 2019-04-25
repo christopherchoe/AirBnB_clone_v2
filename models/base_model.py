01b066df4cd801f3d53c3c63dd683ab156dd418a
christopherchoe <chchchoe@gmail.com>
2019-04-25 02:23:58 +0000

    fixed issues with datetimes not being set at init

--- models/base_model.py
+++ models/base_model.py
@@ -29,6 +29,7 @@ class BaseModel:
             created_at: creation date
             updated_at: updated date
         """
+        self.created_at = self.updated_at = datetime.now()
         if kwargs:
             if 'id' not in kwargs.items():
                 self.id = str(uuid.uuid4())
@@ -39,7 +40,6 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string
